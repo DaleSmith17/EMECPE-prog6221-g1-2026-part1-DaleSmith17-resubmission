32867b96a43d06c9742aca5479f3db2205377bf5
Dale-Smith17 <dalessmith17082006@gmail.com>
2026-04-23 23:05:11 +0200

    Add project files.

---
+++ PROG_Pt1_RESUBMISSION/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PROG_Pt1_RESUBMISSION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
