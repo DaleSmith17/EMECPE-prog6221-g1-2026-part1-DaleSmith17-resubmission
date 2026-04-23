c9474ce33e67112943365aa53d29248a88fe855f
Dale-Smith17 <dalessmith17082006@gmail.com>
2026-04-23 23:19:27 +0200

    Downloaded nuget package for audio file as well as add the code to the class

--- PROG_Pt1_RESUBMISSION/AudioPlayer.cs
+++ PROG_Pt1_RESUBMISSION/AudioPlayer.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Media;
 using System.Text;
 
 namespace PROG_Pt1_RESUBMISSION
 {
-    internal class AudioPlayer
+    // Plays the chatbot greeting audio when the application starts.
+    public class AudioPlayer
     {
+        // Loads and plays the greeting WAV file.
+        public void PlayGreeting()
+        {
+            try
+            {
+                string filepath = "C:/Users/dales/OneDrive/Documents/Sound Recordings/PROG6221 Greeting.wav";
+
+                if (File.Exists(filepath))
+                {
+                    //SoundPlayer is used becuase Micorsoft documents it as a class
+                    //for loading and playing WAV files in .NET applications (Microsoft, 2025).
+                    SoundPlayer player = new SoundPlayer(filepath);
+                    player.Load();
+                    player.PlaySync();
+                }
+                else
+                {
+                    Console.WriteLine("The voice greeting WAV file was not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error playing audio: " + ex.Message);
+            }
+        }
     }
 }
