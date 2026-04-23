3dd42d2448360cc3bd48506995d2671d4fcbf0c0
Dale-Smith17 <dalessmith17082006@gmail.com>
2026-04-23 23:16:22 +0200

    Added ascii art code

--- PROG_Pt1_RESUBMISSION/AsciiArtDisplay.cs
+++ PROG_Pt1_RESUBMISSION/AsciiArtDisplay.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace PROG_Pt1_RESUBMISSION
 {
-    internal class AsciiArtDisplay
+    public class AsciiArtDisplay
     {
+        public void ShowLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(@"
+_________        ___.                 _________                          .__  __          
+\_   ___ \___.__.\_ |__   ___________/   _____/ ____   ____  __ _________|__|/  |_ ___.__.
+/    \  \<   |  | | __ \_/ __ \_  __ \_____  \_/ __ \_/ ___\|  |  \_  __ \  \   __<   |  |
+\     \___\___  | | \_\ \  ___/|  | \/        \  ___/\  \___|  |  /|  | \/  ||  |  \___  |
+ \______  / ____| |___  /\___  >__| /_______  /\___  >\___  >____/ |__|  |__||__|  / ____|
+        \/\/          \/     \/             \/     \/     \/                       \/     
+            ");
+            Console.ResetColor();
+        }
     }
 }
